4b6c5ca3ca0524f8219a6a82ecb7c763f967a179
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-26 20:11:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Demo_Asyn_Await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Asyn<task> and Await<task> keyword");
+            Method1();
+            Method2();
+            Console.ReadKey();
+
+        }
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("Method1");
+                    Task.Delay(100).Wait();
+
+                }
+            });
+
+        }
+        public static void Method2()
+        {
+            for(int i=0;i<25;i++)
+            {
+                Console.WriteLine("Method2");
+                Task.Delay(100).Wait();
+            }
+        }
+    }
+}
